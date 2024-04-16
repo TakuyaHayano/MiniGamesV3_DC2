1e902dd259ceeaae3037f34b488c4c224b2ec431
一輝 岩村 <kazuki.2004.10@outlook.jp>
2024-04-16 11:24:52 +0900

    修正２

--- GAME01/GAME01.cpp
+++ GAME01/GAME01.cpp
@@ -5,6 +5,9 @@ namespace GAME01
 {
 	int GAME::create()
 	{
+		State = TITLE;
+
+
 		return 0;
 	}
 
@@ -12,17 +15,73 @@ namespace GAME01
 	{
 
 	}
-	//aaa
+
+
 	void GAME::proc()
 	{
+		if (State == TITLE)Title();
+		else if (State == PLAY)Play();
+		else if (State == CLEAR)Clear();
+	}
+
+	void GAME::Title() {
+
+
+
+
 		clear(0, 0, 64);
-		textSize(50);
 		fill(255, 255, 0);
+		textSize(50);
 		text("GAME01", 0, 100);
 		fill(255);
 		text("ENTERキーでメニューに戻る", 0, 1080);
+		print("title");
+
+
+		if (isTrigger(MOUSE_LBUTTON)) {
+			Init();
+			State = PLAY;
+			return;
+		}
+
 		if (isTrigger(KEY_ENTER)) {
 			main()->backToMenu();
 		}
+
 	}
+
+	void GAME::Init() {
+
+
+		ClearFlag = false;
+	}
+
+	void GAME::Play() {
+
+		clear(0, 0, 255);
+		fill(255);
+
+		fill(255, 255, 255);
+		print("play");
+		print(" クリックでクリア");
+
+		if (ClearFlag) {
+			State = CLEAR;
+		}
+
+	}
+
+	void GAME::Clear() {
+
+		clear(0, 0, 255);
+		fill(255, 255, 255);
+		print("GAME CLEAR");
+		print("　クリックでタイトルに戻る");
+		if (isTrigger(MOUSE_LBUTTON)) {
+			State = TITLE;
+		}
+	}
+
+
+
 }
--- GAME01/GAME01.h
+++ GAME01/GAME01.h
@@ -2,14 +2,23 @@
 #include "../MAIN/GAME_BASE.h"
 namespace GAME01
 {
-    class GAME :
-        public GAME_BASE
-    {
-    public:
-        GAME(class MAIN* main) :GAME_BASE(main) {};
-        ~GAME() {};
-        int create();
-        void proc();
-        void destroy();
-    };
+	class GAME :
+		public GAME_BASE
+	{
+	public:
+		GAME(class MAIN* main) :GAME_BASE(main) {};
+		~GAME() {};
+		int create();
+		void proc();
+		void destroy();
+	private:
+		enum STATE { TITLE, PLAY, CLEAR, OVER };
+		STATE State = TITLE;
+		void Title();
+		void Init();
+		void Play();
+		void Clear();
+		bool ClearFlag = false;
+
+	};
 }
