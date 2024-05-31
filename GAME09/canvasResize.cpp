e42df09381d6c96b79d358b2a3262e203cb973c1
晴輝 松山 <matu4034@gmail.com>
2024-05-31 13:58:54 +0900

    mod canvasResize.h

--- GAME09/canvasResize.cpp
+++ GAME09/canvasResize.cpp
@@ -1,33 +1,34 @@
 #include "canvasResize.h"
 
-
-VECTOR2 cornerPos = VECTOR2(0,0);
-float magnification = 1.0f;
-
 #undef image
 #undef line
 
-void drawCanvasImage(int idx, float x, float y, float angle, float size) {
-	float X = cornerPos.x + x * magnification;
-	float Y = cornerPos.y + y * magnification;
-	float Size = size * magnification;
-	image(idx, X, Y, angle, Size);
-}
+namespace GAME09 {
+	VECTOR2 cornerPos = VECTOR2(0, 0);
+	float magnification = 1.0f;
 
-void drawCanvasLine(float sx, float sy, float ex, float ey) {
-	
-	float Sx = cornerPos.x + sx * magnification;
-	float Sy = cornerPos.y + sy * magnification;
-	float Ex = cornerPos.x + ex * magnification;
-	float Ey = cornerPos.y + ey * magnification;
-	line(Sx, Sy, Ex, Ey);
-}
+	void drawCanvasImage(int idx, float x, float y, float angle, float size) {
+		float X = cornerPos.x + x * magnification;
+		float Y = cornerPos.y + y * magnification;
+		float Size = size * magnification;
+		image(idx, X, Y, angle, Size);
+	}
 
-void setCornerPos(VECTOR2 pos) {
-	cornerPos = pos;
-}
-void setMagnification(float value) {
-	magnification = value;
+	void drawCanvasLine(float sx, float sy, float ex, float ey) {
+
+		float Sx = cornerPos.x + sx * magnification;
+		float Sy = cornerPos.y + sy * magnification;
+		float Ex = cornerPos.x + ex * magnification;
+		float Ey = cornerPos.y + ey * magnification;
+		line(Sx, Sy, Ex, Ey);
+	}
+
+	void setCornerPos(VECTOR2 pos) {
+		cornerPos = pos;
+	}
+	void setMagnification(float value) {
+		magnification = value;
+	}
 }
 
 #define image drawCanvasImage
--- GAME09/canvasResize.h
+++ GAME09/canvasResize.h
@@ -5,7 +5,9 @@
 #define image drawCanvasImage
 #define line drawCanvasLine
 
-void drawCanvasImage(int idx, float x, float y, float angle = 0, float size = 1);
-void drawCanvasLine(float sx, float sy, float ex, float ey);
-void setCornerPos(VECTOR2 pos);
-void setMagnification(float value);
+namespace GAME09 {
+	void drawCanvasImage(int idx, float x, float y, float angle = 0, float size = 1);
+	void drawCanvasLine(float sx, float sy, float ex, float ey);
+	void setCornerPos(VECTOR2 pos);
+	void setMagnification(float value);
+}
