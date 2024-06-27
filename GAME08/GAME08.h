81442c660a4f323f5ab527708091e94580456b9a
桃香 福田 <momoka_moka@icloud.com>
2024-06-27 15:21:27 +0900

    complete

--- GAME08/GAME08.h
+++ GAME08/GAME08.h
@@ -11,5 +11,80 @@ namespace GAME08
         int create();
         void proc();
         void destroy();
+        void title();
+        void play();
+        void result();
+        void init();
+        void draw();
+
+    private:
+        enum {
+            TITLE,
+            PLAY,
+            RESULT
+        };
+        int hp;
+
+        int TitleBackImg, TitleImg,TitleItemImg, ConfigBackImg , ConfigsImg;
+
+        int FPinkImg;
+
+        bool IsConfig = false;
+
+        int PlayBackImg , KumoPinkImg , KumoBlueImg, KumoPurpleImg;
+
+        int SImg, AImg, BImg, CImg;
+
+        int GClearImg , GOverImg;
+
+        int ClearBackImg, OverBackImg;
+
+        struct CHARACTER {
+            int Character;
+            int Img[2];
+            float Px, Py, Speed;
+            int Life;
+        };
+
+        struct OBJECT {
+            int Object;
+            int Img[6];
+            float Px, Py, Angle, FallSpeed;
+            int Hp;
+        };
+
+
+        struct SQUARE {
+            float x, y, w, h, Speed;
+            float left, right, top, bottom;
+        };
+
+        struct SCORE {
+            int Score;
+        };
+
+        struct OBJECTS {
+            int Objects;
+            int Squares;
+        };
+
+        float LE = 793.0;
+        float RE = 1126.5;
+
+        int STATE = TITLE;
+
+        struct CHARACTER Player;
+        struct OBJECT Item;
+
+        struct SQUARE ap;
+        struct SQUARE bc;
+
+        struct SCORE Ps;
+        struct SCORE Is;
+
+        struct OBJECTS Items[3];
+
+
+
     };
 }
